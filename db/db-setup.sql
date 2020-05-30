0cb47190227634299324185a0f17202e6a843a33
Wilson Neto <contato@wilsonneto.com.br>
2020-05-30 15:51:44 -0300

    sending to github

---
+++ db/db-setup.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE IF NOT EXISTS beers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    brand VARCHAR(50) NOT NULL,
+    is_premium BOOLEAN,
+    registration_date TIMESTAMP
+)
